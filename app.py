b3dd1d0cef0a6eea0093038c604478f65da87374
Williams-W <95378167+Williams-W@users.noreply.github.com>
2024-11-19 00:07:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def summarize_text(text, num_paragraphs=7):  # Change num_paragraphs to 7
     preprocessed_paragraphs = preprocess_text(text)
     
     # Convert preprocessed paragraphs to TF-IDF matrix
-    vectorizer = TfidfVectorizer(ngram_range=(1, 2), min_df=0.05 , max_df=0.8)
+    vectorizer = TfidfVectorizer(ngram_range=(1, 3), min_df=0.05 , max_df=0.85)
     tfidf_matrix = vectorizer.fit_transform(preprocessed_paragraphs)
 
     # Get vocabulary
